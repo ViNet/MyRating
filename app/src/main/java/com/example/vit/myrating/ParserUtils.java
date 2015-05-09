c049162e981c990bbe4a811466fe36ed0521b913
Vitaliy Netetskiy <xsxs2ua@gmail.com>
2015-05-09 14:49:04 +0300

    add Subject class, modify ParserUtils to fill list of Subjects

--- app/src/main/java/com/example/vit/myrating/ParserUtils.java
+++ app/src/main/java/com/example/vit/myrating/ParserUtils.java
@@ -7,7 +7,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.security.auth.Subject;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Vit on 09.05.2015.
@@ -26,16 +28,18 @@ public class ParserUtils {
     static final int NUM_OF_MODULE_FIELDS = 3;
 
     //input argument page - html page downloaded from wev
-    public static void parsePage(String page){
+    public static void parsePage(String page) {
 
         Document doc = Jsoup.parse(page);
 
         int semester = 1;
 
+        List<Subject> subjects = new ArrayList<>();
+
         //get tables with rating by tag <tbody>
         Elements tables = doc.getElementsByTag("tbody");
         //check every table
-        for(Element table : tables) {
+        for (Element table : tables) {
 
             // if there are less than 2 rows in table then the table
             // hasn't subjects
@@ -44,10 +48,9 @@ public class ParserUtils {
 
             for (Element row : table.select("tr")) {
 
-                Log.d(TAG, CLASS + " subject title - " + row.child(SUBJECT_TITLE_ID).text());
-                Log.d(TAG, CLASS + " subject type - " + row.child(SUBJECT_TYPE_ID).text());
-                Log.d(TAG, CLASS + " subject total mark - " + row.child(SUBJECT_TOTAL_MARK_ID).text());
-
+                //Create array list of modules of current subject
+                List<Subject.Module> modules = new ArrayList<Subject.Module>();
+                // first fill the module array list
                 for (int i = SUBJECT_FIRST_MODULE_ID; i <= SUBJECT_LAST_MODULE_ID; i += NUM_OF_MODULE_FIELDS) {
                     // position:
                     // i = module percentage
@@ -59,14 +62,39 @@ public class ParserUtils {
                         continue;
                     }
 
-                    Log.d(TAG, CLASS + "module percentage - " + row.child(i).text());
-                    Log.d(TAG, CLASS + "module date - " + row.child(i + 1).text());
-                    Log.d(TAG, CLASS + "module mark - " + row.child(i + 2).text());
+                    //Log.d(TAG, CLASS + "module percentage - " + row.child(i).text());
+                    //Log.d(TAG, CLASS + "module date - " + row.child(i + 1).text());
+                    //Log.d(TAG, CLASS + "module mark - " + row.child(i + 2).text());
+
+                    modules.add(new Subject.Module(
+                            row.child(i).text()
+                            , row.child(i + 1).text()
+                            , row.child(i + 2).text()
+                    ));
 
                 }
+                //Log.d(TAG, CLASS + " subject title - " + row.child(SUBJECT_TITLE_ID).text());
+                //Log.d(TAG, CLASS + " subject type - " + row.child(SUBJECT_TYPE_ID).text());
+                //Log.d(TAG, CLASS + " subject total mark - " + row.child(SUBJECT_TOTAL_MARK_ID).text());
 
+                //put current subject to the subjects array list
+                subjects.add(
+                        new Subject(
+                                row.child(SUBJECT_TITLE_ID).text()
+                                ,row.child(SUBJECT_TYPE_ID).text()
+                                ,row.child(SUBJECT_TOTAL_MARK_ID).text()
+                                ,semester
+                                ,modules
+                        )
+                );
             }
+            // go to the table of next semester
+            semester++;
 
+        }
+
+        for(Subject subject : subjects){
+            subject.print();
         }
     }
 }
---
+++ app/src/main/java/com/example/vit/myrating/Subject.java
@@ -0,0 +1,71 @@
+package com.example.vit.myrating;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Vit on 09.05.2015.
+ * Class used for saved parsed html data in comfortable structure
+ */
+public class Subject {
+
+    static final String TAG = "myrating";
+    static final String CLASS = Subject.class.getSimpleName() + ": ";
+
+    private String title;
+    private String type;
+    private String totalMark;
+    private int semester;
+    private List<Module> modules;
+    boolean isCompleted;
+
+    Subject(String name, String type, String totalMark, int semester, List<Module> modules){
+        this.title = name;
+        this.type = type;
+        this.totalMark = totalMark;
+        this.semester = semester;
+        this.modules = modules;
+        this.isCompleted = this.isCompleted();
+    }
+
+
+    private boolean isCompleted(){
+        int completedModules = 0;
+        for(int i=0; i<modules.size(); i++){
+            if(!modules.get(i).mark.isEmpty())
+                completedModules++;
+        }
+        return (completedModules == modules.size());
+    }
+
+    // for debug
+    public void print(){
+        Log.d(TAG, CLASS + "title: " + this.title);
+        Log.d(TAG, CLASS + "type: " + this.type);
+        Log.d(TAG, CLASS + "total: " + this.totalMark);
+        Log.d(TAG, CLASS + "semester: " + this.semester);
+        Log.d(TAG, CLASS + "completed: " + this.isCompleted);
+        for(int i=0; i<modules.size(); i++){
+            modules.get(i).print();
+        }
+    }
+
+    public static class Module{
+        String percentage;
+        String date;
+        String mark;
+
+        public Module(String percentage, String date, String mark){
+            this.percentage = percentage;
+            this.date = date;
+            this.mark = mark;
+        }
+
+        //for debug
+        public void print(){
+            Log.d(TAG, CLASS + "p=" + percentage + ", d=" + date + ", m=" + mark);
+        }
+    }
+}
