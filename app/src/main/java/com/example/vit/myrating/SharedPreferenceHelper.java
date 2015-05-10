2f4edf777d17e51d91007c31596d67b422c26c9d
Vitaliy Netetskiy <xsxs2ua@gmail.com>
2015-05-10 14:19:51 +0300

    modify SharedPreferenceHelper for store cookies

--- app/src/main/java/com/example/vit/myrating/SharedPreferenceHelper.java
+++ app/src/main/java/com/example/vit/myrating/SharedPreferenceHelper.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import org.apache.http.cookie.Cookie;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,9 +22,10 @@ public class SharedPreferenceHelper {
 
     static final String PREFERENCE_NAME = "currentUser";
     static final String KEY_SUBJECTS_LIST = "subjectsList";
+    static final String KEY_COOKIES = "cookies";
 
 
-    static public void saveToSharedPreference(Context context, List data){
+    static public void storeSubjectList(Context context, List data){
         // used for store arrayList in json format
         SharedPreferences preferences = context.
                 getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
@@ -36,7 +39,7 @@ public class SharedPreferenceHelper {
         Log.d(TAG, CLASS + " saved in shared preference");
     }
 
-    static public List<Subject> getFromSharedPreference(Context context){
+    static public List<Subject> getSubjectList(Context context){
         // used for retrieving arraylist from json formatted string
         List subjects = null;
         SharedPreferences preferences = context.
@@ -51,5 +54,41 @@ public class SharedPreferenceHelper {
         }
         Log.d(TAG, CLASS + " load in shared preference");
         return subjects;
+    }
+
+    static public void storeCookies(Context context, List<Cookie> cookies ){
+        SharedPreferences preferences = context.
+                getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(cookies);
+
+        editor.putString(KEY_COOKIES,json);
+        editor.apply();
+        Log.d(TAG, CLASS + " stored cookies in shared preferences");
+    }
+
+    static public List<Cookie> getCookies(Context context){
+        // used for retrieving arraylist from json formatted string
+        List cookies = null;
+        SharedPreferences preferences = context.
+                getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        if(preferences.contains(KEY_COOKIES)){
+            String json = preferences.getString(KEY_COOKIES, null);
+            Gson gson = new Gson();
+            Cookie[] cookiesArray = gson.fromJson(json, Cookie[].class);
+            cookies = Arrays.asList(cookiesArray);
+            cookies = new ArrayList<Cookie>(cookies);
+        }
+        Log.d(TAG, CLASS + " loaded cookies from shared preference");
+        return cookies;
+    }
+
+    static public boolean isCookieExist(Context context){
+        SharedPreferences preferences = context.
+                getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        return preferences.contains(KEY_COOKIES);
     }
 }
