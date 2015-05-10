2de885bc059b37ff2e70c54733f5d14276a11c11
Vitaliy Netetskiy <xsxs2ua@gmail.com>
2015-05-10 14:30:18 +0300

    modify splash screen

--- app/src/main/java/com/example/vit/myrating/SplashActivity.java
+++ app/src/main/java/com/example/vit/myrating/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.vit.myrating;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,23 +14,35 @@ import android.widget.Button;
 
 public class SplashActivity extends ActionBarActivity {
 
-    Button btnToLoginActivity;
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        btnToLoginActivity = (Button) findViewById(R.id.btnToLoginActivity);
-
-        btnToLoginActivity.setOnClickListener(new View.OnClickListener() {
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class)
-                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            public void run() {
+            // if already exist some cookies then start main activity
+                if(SharedPreferenceHelper.isCookieExist(getBaseContext())){
+                    startActivity(
+                            new Intent(SplashActivity.this, MainActivity.class).
+                                    setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+                    );
+                } else{
+                    // there are no logged users, show login activity
+                    startActivity(
+                            new Intent(SplashActivity.this, LoginActivity.class).
+                                    setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+                    );
+                }
                 finish();
             }
-        });
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
 }
