b6cf15a6aa4d013ace5f6722a849dfaed38b3aaf
Vitaliy Netetskiy <xsxs2ua@gmail.com>
2015-05-10 15:16:15 +0300

    add logout menu

--- app/src/main/java/com/example/vit/myrating/MainActivity.java
+++ app/src/main/java/com/example/vit/myrating/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.vit.myrating;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,6 +11,9 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String TAG = "myrating";
+    static final String CLASS = MainActivity.class.getSimpleName() + ": ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +35,19 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        Log.d(TAG, CLASS + " onOptionsItemSelected()");
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            //delete user and show login activity
+            Log.d(TAG, CLASS + " logout");
+            SharedPreferenceHelper.deleteCurrentUser(getBaseContext());
+            startActivity(
+                    new Intent(MainActivity.this, LoginActivity.class).
+                            setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+            );
+            finish();
             return true;
         }
 
